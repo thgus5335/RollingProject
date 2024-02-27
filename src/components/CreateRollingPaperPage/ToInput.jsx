7cebf14a6624f4718993f8b1e0e99cbdf80d9a97
정기환 <joelkh784@gmail.com>
2024-02-28 02:29:26 +0900

    Feat: post user name and background color to server

--- src/components/CreateRollingPaperPage/ToInput.jsx
+++ src/components/CreateRollingPaperPage/ToInput.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import styles from './ToInput.module.css';
 
-const ToInput = () => {
+const ToInput = ({ onNameChange }) => {
   const [inputValue, setInputValue] = useState('');
   const [error, setError] = useState(false);
 
   const handleInputChange = e => {
     setInputValue(e.target.value);
     setError(false);
+    onNameChange(e.target.value);
   };
 
   const handleInputBlur = () => {
@@ -17,6 +18,7 @@ const ToInput = () => {
   };
 
   const placeholder = error ? '값을 입력해 주세요.' : '받는 사람 이름을 입력해 주세요.';
+
   return (
     <div>
       <h1>To.</h1>
--- src/pages/CreateRollingPaperPage.jsx
+++ src/pages/CreateRollingPaperPage.jsx
@@ -4,9 +4,10 @@ import checkIcon from '../assets/icons/check.svg';
 import car from '../assets/images/car.png';
 import arches from '../assets/images/arches.png';
 import { useState } from 'react';
+import postRolling from '../apis/createRollingAPI';
 
 const CreateRollingPaperPage = () => {
-  const COLORS = ['orange', 'purple', 'blue', 'green'];
+  const COLORS = ['beige', 'purple', 'blue', 'green'];
   const IMAGES = [
     { alt: 'car', source: car },
     { alt: 'arches', source: arches },
@@ -19,9 +20,10 @@ const CreateRollingPaperPage = () => {
       source: 'https://i0.wp.com/regroove.ca/wp-content/uploads/2021/10/pexels-photo-2607544.jpeg?fit=1024%2C682&ssl=1',
     },
   ];
-  const [selectedColor, setSelectedColor] = useState('');
+  const [selectedColor, setSelectedColor] = useState('beige');
   const [selectedImage, setSelectedImage] = useState('');
   const [selectedButton, setSelectedButton] = useState('color');
+  const [name, setName] = useState('');
 
   const handleClickColorButton = () => {
     setSelectedButton('color');
@@ -38,9 +40,26 @@ const CreateRollingPaperPage = () => {
     setSelectedImage(img);
   };
 
+  const handleNameChange = inputValue => {
+    setName(inputValue);
+  };
+
+  const handleCreateButtonClick = () => {
+    // Create a postData object with the necessary data
+    const postData = {
+      name: name,
+      backgroundColor: selectedColor,
+      // Add other properties as needed
+    };
+
+    // Call the postRolling function with the postData
+    postRolling(postData);
+    console.log(postData);
+  };
+
   return (
     <main>
-      <ToInput />
+      <ToInput onNameChange={handleNameChange} />
 
       <div>
         <h1>배경화면을 선택해 주세요.</h1>
@@ -79,7 +98,9 @@ const CreateRollingPaperPage = () => {
         </div>
       )}
 
-      <button className={styles.createButton}>생성하기</button>
+      <button onClick={handleCreateButtonClick} className={styles.createButton}>
+        생성하기
+      </button>
     </main>
   );
 };
