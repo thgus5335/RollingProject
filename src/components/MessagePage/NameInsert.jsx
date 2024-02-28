bc11f952291036f476ef83233e0340fc0d46f30b
hyunjun9788 <namhyunjun9788@gmail.com>
2024-02-29 00:29:52 +0900

    Feat: Modify Image Selection Functionality

--- src/components/MessagePage/NameInsert.jsx
+++ src/components/MessagePage/NameInsert.jsx
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import styles from './NameInsert.module.css';
 const Name = () => {
+  const [name, setName] = useState('');
+
+  const handleChangeName = () => {
+    setName(name);
+  };
   return (
     <div className={styles.nameContainer}>
       <h1>From.</h1>
-      <input className={styles.nameInput} placeholder="이름을 입력해 주세요."></input>
+      <input
+        value={name}
+        className={styles.nameInput}
+        placeholder="이름을 입력해 주세요."
+        onChange={handleChangeName}></input>
     </div>
   );
 };
--- src/components/MessagePage/ProfileImg.jsx
+++ src/components/MessagePage/ProfileImg.jsx
@@ -1,19 +1,9 @@
 import profileImg1 from '../../assets/images/profile-img1.png';
 import profileImg2 from '../../assets/images/profile-img2.png';
+import profileImg3 from '../../assets/images/profile-img3.png';
 import styles from './ProfileImg.module.css';
 const ProfileImg = () => {
-  const images = [
-    profileImg1,
-    profileImg2,
-    profileImg1,
-    profileImg2,
-    profileImg1,
-    profileImg2,
-    profileImg1,
-    profileImg2,
-    profileImg1,
-    profileImg2,
-  ];
+  const images = [profileImg1, profileImg2, profileImg3];
 
   return (
     <div className={styles.profileImgList}>
