6e68e501fd1c73eb6bcbd17e1a50cbbbc65ada39
정기환 <joelkh784@gmail.com>
2024-02-28 23:31:02 +0900

    Test: upload img to the server

--- src/apis/createRollingAPI.jsx
+++ src/apis/createRollingAPI.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
+const API_URL = 'https://rolling-api.vercel.app/4-13/recipients/';
 
-const postRolling = async postData => {
-  const API_URL = 'https://rolling-api.vercel.app/4-13/recipients/';
-
+export const postRolling = async postData => {
   try {
+    console.log('Sending request with data:', postData);
     const response = await axios.post(API_URL, postData, {
       headers: {
         'Content-Type': 'application/json',
@@ -20,4 +20,29 @@ const postRolling = async postData => {
   }
 };
 
-export default postRolling;
+export const getUserId = async userName => {
+  try {
+    const response = await axios.get(API_URL, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    console.log('Success:', response.data.results);
+
+    const user = response.data.results.find(user => user.name === userName);
+
+    if (user) {
+      // Return the corresponding user ID if found
+      return user.id;
+    } else {
+      // Handle the case where the user with the given name is not found
+      console.error('User not found for name:', userName);
+      return null;
+    }
+  } catch (error) {
+    console.error('Error:', error.response ? error.response.data : error.message);
+
+    throw error;
+  }
+};
--- src/pages/CreateRollingPaperPage.jsx
+++ src/pages/CreateRollingPaperPage.jsx
@@ -4,27 +4,29 @@ import checkIcon from '../assets/icons/check.svg';
 import car from '../assets/images/car.png';
 import arches from '../assets/images/arches.png';
 import { useState } from 'react';
-import postRolling from '../apis/createRollingAPI';
+import { postRolling } from '../apis/createRollingAPI';
+// import { getUserId } from '../apis/createRollingAPI';
 
 const CreateRollingPaperPage = () => {
   const COLORS = ['beige', 'purple', 'blue', 'green'];
   const IMAGES = [
     { alt: 'car', source: car },
     { alt: 'arches', source: arches },
-    {
-      alt: 'cat',
-      source: 'https://static.india.com/wp-content/uploads/2015/10/8-wallpaperhd1.jpg',
-    },
-    {
-      alt: 'dog',
-      source: 'https://i0.wp.com/regroove.ca/wp-content/uploads/2021/10/pexels-photo-2607544.jpeg?fit=1024%2C682&ssl=1',
-    },
+    // {
+    //   alt: 'cat',
+    //   source: 'https://static.india.com/wp-content/uploads/2015/10/8-wallpaperhd1.jpg',
+    // },
+    // {
+    //   alt: 'dog',
+    //   source: 'https://i0.wp.com/regroove.ca/wp-content/uploads/2021/10/pexels-photo-2607544.jpeg?fit=1024%2C682&ssl=1',
+    // },
   ];
   const [selectedColor, setSelectedColor] = useState('beige');
-  const [selectedImage, setSelectedImage] = useState('');
+  const [selectedImage, setSelectedImage] = useState('car');
+
   const [selectedButton, setSelectedButton] = useState('color');
   const [name, setName] = useState('');
-  const [imgFile, setImgFile] = useState(null);
+  const [imgURL, setImgURL] = useState('');
 
   const handleClickColorButton = () => {
     setSelectedButton('color');
@@ -38,11 +40,11 @@ const CreateRollingPaperPage = () => {
     setSelectedColor(color);
   };
 
-  const handleFileChange = e => {
+  const handleImageUpload = e => {
     const uploadedFile = e.target.files[0];
-    setImgFile(uploadedFile);
-    setSelectedImage(URL.createObjectURL(uploadedFile));
-    console.log(selectedImage);
+    setImgURL(URL.createObjectURL(uploadedFile));
+    console.log(imgURL);
+    console.log(uploadedFile);
   };
 
   const handleClickImage = img => {
@@ -54,18 +56,42 @@ const CreateRollingPaperPage = () => {
   };
 
   const handleCreateButtonClick = () => {
-    // Create a postData object with the necessary data
     const postData = {
       name: name,
       backgroundColor: selectedColor,
-      backgroundImageURL: imgFile,
-      // Add other properties as needed
+      // backgroundImageURL: 'https://static.india.com/wp-content/uploads/2015/10/8-wallpaperhd1.jpg',
+      // local host imgURL not working
+      backgroundImageURL: imgURL,
     };
 
-    // Call the postRolling function with the postData
     postRolling(postData);
     console.log(postData);
   };
+
+  // const handleCreateButtonClick = async () => {
+  //   try {
+  //     const existingUserId = await getUserId(name);
+  //     console.log(existingUserId);
+
+  //     const postData = {
+  //       name: name,
+  //       backgroundColor: selectedColor,
+
+  //     };
+
+  //     if (existingUserId) {
+  //       postData.userId = existingUserId;
+
+  //       await postRolling(postData);
+  //       console.log('User data updated successfully.');
+  //     } else {
+  //       await postRolling(postData);
+  //       console.log('New user data created successfully.');
+  //     }
+  //   } catch (error) {
+  //     console.error('Failed to create or update user data:', error);
+  //   }
+  // };
 
   return (
     <main>
@@ -94,23 +120,34 @@ const CreateRollingPaperPage = () => {
           ))}
         </div>
       ) : (
-        <div className={styles.pickerContainer}>
-          {IMAGES.map(images => (
-            <div key={images.alt} className={`${styles.pickerBox} `} onClick={() => handleClickImage(images.alt)}>
-              <img
-                src={images.source}
-                alt={images.alt}
-                className={selectedImage === images.alt && styles.imagesOpacity}
-              />
-              {selectedImage === images.alt && <img src={checkIcon} alt="check" className={styles.check} />}
-            </div>
-          ))}
+        <div>
+          <div className={styles.pickerContainer}>
+            {IMAGES.map(images => (
+              <div key={images.alt} className={`${styles.pickerBox} `} onClick={() => handleClickImage(images.alt)}>
+                <img
+                  src={images.source}
+                  alt={images.alt}
+                  className={selectedImage === images.alt && styles.imagesOpacity}
+                />
+
+                {selectedImage === images.alt && <img src={checkIcon} alt="check" className={styles.check} />}
+              </div>
+            ))}
+            {imgURL.length > 0 && (
+              <div className={`${styles.pickerBox} `} onClick={() => handleClickImage(imgURL)}>
+                <img src={imgURL} alt="userImage" className={selectedImage === imgURL && styles.imagesOpacity} />
+                {selectedImage === imgURL && <img src={checkIcon} alt="check" className={styles.check} />}
+              </div>
+            )}
+            <input type="file" accept="image/*" onChange={handleImageUpload} className={styles.pickerBox} />
+          </div>
         </div>
       )}
-      <input type="file" accept="image/*" onChange={handleFileChange} />
-      <img src={selectedImage} alt="userImage" />
 
-      <button onClick={handleCreateButtonClick} className={styles.createButton}>
+      <button
+        onClick={handleCreateButtonClick}
+        disabled={name.length === 0}
+        className={`${styles.createButton} ${name.length > 0 && styles.activateButton}`}>
         생성하기
       </button>
     </main>
