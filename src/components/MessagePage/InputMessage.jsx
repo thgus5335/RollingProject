348eae63c9bed24cfb2a9d0e3f316a7003f5f1e9
hyunjun9788 <namhyunjun9788@gmail.com>
2024-02-29 13:14:00 +0900

    Feat: feat: Add functionality to display pure text by removing HTML tags in ReactQuill

--- src/components/MessagePage/InputMessage.jsx
+++ src/components/MessagePage/InputMessage.jsx
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import styles from './InputMessage.module.css';
 import ReactQuill from 'react-quill';
 const InputMessage = () => {
+  const [content, setContent] = useState('');
+  const pureText = content.replace(/(<([^>]+)>)/gi, '');
+  console.log(pureText);
+  const handleChangeContent = text => {
+    setContent(text);
+  };
   return (
     <div className={styles.container}>
       <h1>내용을 입력해 주세요.</h1>
-      <ReactQuill style={{ width: '45rem', height: '19.25rem' }} />
+      <ReactQuill onChange={handleChangeContent} style={{ width: '45rem', height: '19.25rem' }} />
     </div>
   );
 };
