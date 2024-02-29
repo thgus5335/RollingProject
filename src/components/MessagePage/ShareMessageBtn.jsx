99da395b6ba47da6076ccc14623486966d2c268f
hyunjun9788 <namhyunjun9788@gmail.com>
2024-03-01 01:18:45 +0900

    Feat: Implement functionality for the 'Create' button

--- src/components/MessagePage/ShareMessageBtn.jsx
+++ src/components/MessagePage/ShareMessageBtn.jsx
@@ -2,7 +2,9 @@ import styles from './ShareMessageBtn.module.css';
 const ShareMessageBtn = ({ isValid }) => {
   return (
     <div className={styles.container}>
-      <button className={isValid ? styles.validButton : styles.invalidButton}>생성하기</button>
+      <button className={isValid ? styles.validButton : styles.invalidButton} disabled={!isValid}>
+        생성하기
+      </button>
     </div>
   );
 };
