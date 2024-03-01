759b035d09225c3deaaabc7aec62e87590875812
hyunjun9788 <namhyunjun9788@gmail.com>
2024-03-01 20:28:28 +0900

    Feat: Implement responsive design for tablet views

--- src/components/MessagePage/InputMessage.jsx
+++ src/components/MessagePage/InputMessage.jsx
@@ -8,7 +8,7 @@ const InputMessage = ({ onMessageChange }) => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>내용을 입력해 주세요.</h1>
-      <ReactQuill onChange={handleChangeContent} style={{ width: '45rem', height: '19.25rem' }} />
+      <ReactQuill onChange={handleChangeContent} style={{ height: '19.25rem' }} />
     </div>
   );
 };
