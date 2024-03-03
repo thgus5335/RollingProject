f390ad5f76d5c04230b9fbeeb48ba9ff04457b17
hyunjun9788 <namhyunjun9788@gmail.com>
2024-03-04 02:49:37 +0900

    Fix invalid image URL

--- src/apis/createMessageApi.jsx
+++ src/apis/createMessageApi.jsx
@@ -19,7 +19,6 @@ export const createMessageApi = async (submitInfo, id) => {
 export const getRecipient = async () => {
   try {
     const response = await axios.get(`${BASE_URL}`);
-    console.log(response.data);
     return response.data;
   } catch (error) {
     console.log(error);
--- src/components/MessagePage/Profile.jsx
+++ src/components/MessagePage/Profile.jsx
@@ -1,16 +1,16 @@
 import styles from './Profile.module.css';
-import customImg from '../../assets/images/custom.png';
+// import customImg from '../../assets/images/custom.png';
 import ProfileImg from './ProfileImg';
-import profileImg1 from '../../assets/images/profile-img1.png';
-import profileImg2 from '../../assets/images/profile-img2.png';
-import profileImg3 from '../../assets/images/profile-img3.png';
+// import profileImg1 from '../../assets/images/profile-img1.png';
+// import profileImg2 from '../../assets/images/profile-img2.png';
+// import profileImg3 from '../../assets/images/profile-img3.png';
 import { useEffect, useState } from 'react';
 const Profile = ({ onProfileUrlChange, profileUrl }) => {
-  const customImgUrl = process.env.PUBLIC_URL + customImg;
+  const customImgUrl = process.env.PUBLIC_URL + '/custom.png';
   const profileImages = [
-    process.env.PUBLIC_URL + profileImg1,
-    process.env.PUBLIC_URL + profileImg2,
-    process.env.PUBLIC_URL + profileImg3,
+    process.env.PUBLIC_URL + '/profile-img1.png',
+    process.env.PUBLIC_URL + '/profile-img2.png',
+    process.env.PUBLIC_URL + '/profile-img3.png',
   ];
   const [isImageClicked, setIsImageClicked] = useState({ custom: true });
 
@@ -31,7 +31,7 @@ const Profile = ({ onProfileUrlChange, profileUrl }) => {
     <div className={styles.profileContainer}>
       <h1 className={styles.title}>프로필 이미지</h1>
       <div className={styles.profileImage}>
-        <img className={styles.customImg} src={customImgUrl} onClick={handleGetUrl} />
+        <img className={styles.customImg} src="/custom.png" onClick={handleGetUrl} />
         <div className={styles.imageSelect}>
           <div>프로필 이미지를 선택해주세요!</div>
           <ProfileImg
