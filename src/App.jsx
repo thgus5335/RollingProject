84a492a3d8ed1efe22b6666254c20c1d712cda2d
DoDOhyeon <118147033+DoDOhyeon@users.noreply.github.com>
2024-03-05 19:07:02 +0900

    Revert "[Dohyun] common폴더 삭제"

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,3 @@
-import './styles/global.css';
-import { RouterProvider } from 'react-router-dom';
-import router from './router';
-function App() {
-  return (
-    <div className="App">
-      <RouterProvider router={router} />
-    </div>
-  );
-}
+function App() {}
 
 export default App;
