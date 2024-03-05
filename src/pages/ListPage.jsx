0499c738face61e582a5065c287fc6cbd525a229
eugene9851 <eugenelee980713@gmail.com>
2024-03-06 03:18:54 +0900

    Fix: arrange files

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,10 +1,20 @@
-import Main from '../components/ListPage/Main';
+import HotList from '../components/ListPage/HotList';
+import Button from '../components/common/Button';
+import RecentList from '../components/ListPage/RecentList';
+import styles from './ListPage.module.css';
+import { Link } from 'react-router-dom';
 
 function ListPage() {
   return (
-    <div>
-      <Main />
-    </div>
+    <main className={styles.outfit}>
+      <HotList />
+      <RecentList />
+      <div className={styles.buttonWidth}>
+        <Link to="/post">
+          <Button size="medium">나도 만들어보기</Button>
+        </Link>
+      </div>
+    </main>
   );
 }
 
