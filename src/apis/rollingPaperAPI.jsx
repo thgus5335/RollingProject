1b44826ff177deab573eafba0d252fcd02d90737
thgus5335 <thgus5335@naver.com>
2024-03-06 04:21:42 +0900

    Style: add background in RollingPaperPage

--- src/apis/rollingPaperAPI.jsx
+++ src/apis/rollingPaperAPI.jsx
@@ -4,13 +4,18 @@ import { BASE_URL } from '../constants/constants';
 const getRequest = async endPoint => {
   try {
     const response = await axios.get(`${BASE_URL}${endPoint}`);
-    return response.data.results;
+    return response.data;
   } catch (error) {
     console.error(error);
     throw error;
   }
 };
 
+export const getRollingPaper = async id => {
+  return getRequest(`${id}/`);
+};
+
 export const getMessage = async id => {
-  return getRequest(`${id}/messages/`);
+  const response = await getRequest(`${id}/messages/`);
+  return response.results;
 };
--- src/pages/RollingPaperPage.jsx
+++ src/pages/RollingPaperPage.jsx
@@ -1,27 +1,43 @@
 import styles from './RollingPaperPage.module.css';
 import CardList from '../components/RollingPaperPage/CardList';
 import Button from '../components/common/Button';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { getRollingPaper } from '../apis/rollingPaperAPI';
 
 const RollingPage = () => {
   const id = useParams();
-  console.log(id);
+  const [rollingInfo, setRollingInfo] = useState([]);
+  const { backgroundColor, backgroundImageURL } = rollingInfo;
+  const background = backgroundImageURL ? 'imageBackground' : backgroundColor;
+
+  const fetchData = async id => {
+    const response = await getRollingPaper(id);
+    setRollingInfo(response);
+  };
+
+  useEffect(() => {
+    fetchData(id.id);
+  }, []);
+
+  console.log(rollingInfo);
 
   return (
-    <>
-      <main>
-        <div className={styles.rollingBackground}>
-          <div className={styles.buttonContainer}>
-            <div className={styles.buttonEdit}>
-              <Button size={'medium'} type="outline">
-                편집하기
-              </Button>
-            </div>
+    <main>
+      <div
+        className={`${styles.rollingBackground} ${styles[background]}`}
+        style={{ backgroundImage: `url(${backgroundImageURL})` }}></div>
+      <div className={styles.rollingContainer}>
+        <div className={styles.buttonContainer}>
+          <div className={styles.buttonEdit}>
+            <Button size={'medium'} type="outline">
+              편집하기
+            </Button>
           </div>
-          <CardList id={id.id} />
         </div>
-      </main>
-    </>
+        <CardList id={id.id} />
+      </div>
+    </main>
   );
 };
 
