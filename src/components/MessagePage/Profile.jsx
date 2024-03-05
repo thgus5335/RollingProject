bf5b68d43873044efc935ebb0dfc42e78f73bbb3
hyunjun9788 <namhyunjun9788@gmail.com>
2024-03-06 02:03:47 +0900

    Refactor: Removed Unused Code and Deleted Partial Images

--- src/components/MessagePage/Profile.jsx
+++ src/components/MessagePage/Profile.jsx
@@ -1,26 +1,17 @@
 import styles from './Profile.module.css';
 import ProfileImg from './ProfileImg';
+// import { useState } from 'react';
 
-import { useState } from 'react';
 const Profile = ({ onProfileUrlChange, profileUrl }) => {
-  // const customImgUrl = 'https://i.imgur.com/u4nrpjo.png';
-  // console.log(customImgUrl);
-
-  const profileImages = [
+  const PROFILE_IMAGES = [
     'https://i.imgur.com/v9GSBUB.png',
     'https://i.imgur.com/rr1x5VQ.png',
     'https://i.imgur.com/wpGcjHh.png',
     'https://i.imgur.com/IereXDn.png',
-    'https://i.imgur.com/fU2kI3U.png',
     'https://i.imgur.com/TOZC5zi.png',
-    'https://i.imgur.com/aOjTFl6.png',
-    'https://i.imgur.com/ROUsh9Z.png',
     'https://i.imgur.com/aIdZMSf.png',
     'https://i.imgur.com/ztVqUdO.png',
   ];
-
-  console.log(profileImages);
-  const [isImageClicked, setIsImageClicked] = useState('https://i.imgur.com/v9GSBUB.png');
 
   const handleGetUrl = e => {
     const newImgUrl = e.target.src;
@@ -28,12 +19,7 @@ const Profile = ({ onProfileUrlChange, profileUrl }) => {
     if (profileUrl !== newImgUrl) {
       onProfileUrlChange(newImgUrl);
     }
-    setIsImageClicked(isImageClicked);
   };
-
-  // useEffect(() => {
-  //   onProfileUrlChange(customImgUrl);
-  // }, []);
 
   return (
     <div className={styles.profileContainer}>
@@ -42,12 +28,7 @@ const Profile = ({ onProfileUrlChange, profileUrl }) => {
         <img className={styles.customImg} src={profileUrl} />
         <div className={styles.imageSelect}>
           <div>프로필 이미지를 선택해주세요!</div>
-          <ProfileImg
-            profileUrl={profileUrl}
-            handleGetUrl={handleGetUrl}
-            imagesUrl={profileImages}
-            setIsImageClicked={setIsImageClicked}
-          />
+          <ProfileImg profileUrl={profileUrl} handleGetUrl={handleGetUrl} imagesUrl={PROFILE_IMAGES} />
         </div>
       </div>
     </div>
