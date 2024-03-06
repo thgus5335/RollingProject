6d232e34381ec79df1b6f21ff2af3649a187c8c9
hyunjun9788 <namhyunjun9788@gmail.com>
2024-03-07 02:42:06 +0900

    Feat: Handle Error for Submitting NameInput with Blank Entry on Enter Press

--- src/components/MessagePage/NameInsert.jsx
+++ src/components/MessagePage/NameInsert.jsx
@@ -11,7 +11,14 @@ const NameInsert = ({ name, onNameChange }) => {
   const handleBlurFromInput = () => {
     setBlur(name.trim().length === 0);
   };
-
+  const handleKeyDown = e => {
+    console.log(e.key);
+    if (e.key === 'Enter' && name === '') {
+      setBlur(true);
+    } else {
+      setBlur(false);
+    }
+  };
   return (
     <div className={styles.nameContainer}>
       <h1 className={styles.title}>From.</h1>
@@ -21,6 +28,7 @@ const NameInsert = ({ name, onNameChange }) => {
         placeholder="이름을 입력해 주세요."
         onChange={handleChangeName}
         onBlur={handleBlurFromInput}
+        onKeyDown={handleKeyDown}
       />
       {blur === true ? <div className={styles.error}>값을 입력해 주세요.</div> : null}
     </div>
