642cd8f4b821b583a79f9e0f77dc89899e12559f
hyunjun9788 <namhyunjun9788@gmail.com>
2024-03-06 18:33:51 +0900

    Feat: Incomplete Implementation of Profile Image Addition Feature

--- src/components/MessagePage/Profile.jsx
+++ src/components/MessagePage/Profile.jsx
@@ -1,8 +1,12 @@
 import styles from './Profile.module.css';
 import ProfileImg from './ProfileImg';
+import plusIcon from '../../assets/icons/plus.svg';
+import stylesImg from './ProfileImg.module.css';
+import deleteIcon from '../../assets/icons/delete.svg';
+import { useState } from 'react';
 // import { useState } from 'react';
-
-const Profile = ({ onProfileUrlChange, profileUrl }) => {
+import uploadImage from '../../apis/uploadImage';
+const Profile = ({ onProfileUrlChange, profileUrl, customImgUrl }) => {
   const PROFILE_IMAGES = [
     'https://i.imgur.com/v9GSBUB.png',
     'https://i.imgur.com/rr1x5VQ.png',
@@ -12,7 +16,17 @@ const Profile = ({ onProfileUrlChange, profileUrl }) => {
     'https://i.imgur.com/aIdZMSf.png',
     'https://i.imgur.com/ztVqUdO.png',
   ];
+  const [isHovered, setIsHovered] = useState(false);
 
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const [imgURL, setImgURL] = useState('');
   const handleGetUrl = e => {
     const newImgUrl = e.target.src;
 
@@ -20,15 +34,70 @@ const Profile = ({ onProfileUrlChange, profileUrl }) => {
       onProfileUrlChange(newImgUrl);
     }
   };
+  const handleImageUpload = async e => {
+    const uploadedFile = e.target.files[0];
 
+    try {
+      const uploadedImageURL = await uploadImage({ file: uploadedFile });
+      // const uploadedImageURL = URL.createObjectURL(uploadedFile);
+      setImgURL(uploadedImageURL);
+      console.log(imgURL);
+    } catch (error) {
+      console.error('Error uploading image:', error);
+    }
+  };
+  const handleDeleteImg = () => {
+    onProfileUrlChange(customImgUrl);
+  };
   return (
     <div className={styles.profileContainer}>
       <h1 className={styles.title}>프로필 이미지</h1>
       <div className={styles.profileImage}>
-        <img className={styles.customImg} src={profileUrl} />
+        <label className={styles.label} htmlFor="imageInput">
+          {isHovered && profileUrl === customImgUrl ? (
+            <img
+              src={plusIcon}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              alt="plus icon"
+              className={`${styles.customImg} ${styles.plusIcon}`}
+            />
+          ) : (
+            <div className={styles.customImgContainer}>
+              {isHovered && profileUrl !== customImgUrl && (
+                <img
+                  className={styles.delete}
+                  src={deleteIcon}
+                  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}
+                  onClick={handleDeleteImg}
+                />
+              )}
+              <img
+                className={styles.customImg}
+                src={profileUrl}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                onClick={e => handleGetUrl(e)}
+              />
+            </div>
+          )}
+          {/* {isHovered && profileUrl !== customImgUrl && <div className={styles.delete}>삭제</div>} */}
+        </label>
+
+        {profileUrl === customImgUrl ? (
+          <input
+            type="file"
+            id="imageInput"
+            accept="image/*"
+            onChange={handleImageUpload}
+            className={` ${stylesImg.input} `}
+          />
+        ) : null}
+
         <div className={styles.imageSelect}>
           <div>프로필 이미지를 선택해주세요!</div>
-          <ProfileImg profileUrl={profileUrl} handleGetUrl={handleGetUrl} imagesUrl={PROFILE_IMAGES} />
+          <ProfileImg imgURL={imgURL} profileUrl={profileUrl} handleGetUrl={handleGetUrl} imagesUrl={PROFILE_IMAGES} />
         </div>
       </div>
     </div>
--- src/components/MessagePage/ProfileImg.jsx
+++ src/components/MessagePage/ProfileImg.jsx
@@ -1,36 +1,8 @@
-// import { useState } from 'react';
-
 import styles from './ProfileImg.module.css';
-import { useState } from 'react';
-import uploadImage from '../../apis/uploadImage';
-import plusIcon from '../../assets/icons/plus.svg';
 
-const ProfileImg = ({ profileUrl, handleGetUrl, imagesUrl }) => {
-  const [imgURL, setImgURL] = useState('');
-  const handleImageUpload = async e => {
-    const uploadedFile = e.target.files[0];
-
-    try {
-      const uploadedImageURL = await uploadImage({ file: uploadedFile });
-      // const uploadedImageURL = URL.createObjectURL(uploadedFile);
-      setImgURL(uploadedImageURL);
-      console.log(imgURL);
-    } catch (error) {
-      console.error('Error uploading image:', error);
-    }
-  };
+const ProfileImg = ({ profileUrl, handleGetUrl, imagesUrl, imgURL }) => {
   return (
     <div className={styles.profileImgList}>
-      <label htmlFor="imageInput">
-        <img src={plusIcon} alt="plus icon" className={styles.image} />
-      </label>
-      <input
-        type="file"
-        id="imageInput"
-        accept="image/*"
-        onChange={handleImageUpload}
-        className={` ${styles.input} `}
-      />
       {imagesUrl.map((imageUrl, index) => (
         <img
           className={`${styles.image} ${profileUrl === imageUrl ? styles.selected : ''}`}
