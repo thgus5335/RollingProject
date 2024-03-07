33b85728d741c9fbd05983d4bf8410c296f0c5c8
thgus5335 <thgus5335@naver.com>
2024-03-07 18:09:24 +0900

    Fix: infinity scroll

--- src/Toast/Toast.jsx
+++ src/Toast/Toast.jsx
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { v4 } from 'uuid';
 import ToastMessage from './ToastMessage';
 
@@ -20,97 +20,64 @@ class Toast {
   // MessageType = 'success' | 'warning' | 'error' | 'info';
   addSuccess = message => {
     const id = v4();
-    if (
-      this.TOAST_QUEUE.filter(toast => toast.message === message).length > 0
-    ) {
+    if (this.TOAST_QUEUE.filter(toast => toast.message === message).length > 0) {
       return;
     }
     this.TOAST_QUEUE.push({
       id,
       message,
-      messageType: 'success'
+      messageType: 'success',
     });
-    this.ROOT_ELEMENT.render(
-      <ToastMessage
-        messages={this.TOAST_QUEUE}
-        closeMessage={this.removeToast}
-      />
-    );
+    this.ROOT_ELEMENT.render(<ToastMessage messages={this.TOAST_QUEUE} closeMessage={this.removeToast} />);
     this.autoRemoveToast(id);
   };
 
   addWarning = message => {
     const id = v4();
-    if (
-      this.TOAST_QUEUE.filter(toast => toast.message === message).length > 0
-    ) {
+    if (this.TOAST_QUEUE.filter(toast => toast.message === message).length > 0) {
       return;
     }
     this.TOAST_QUEUE.push({
       id,
       message,
-      messageType: 'warning'
+      messageType: 'warning',
     });
-    this.ROOT_ELEMENT.render(
-      <ToastMessage
-        messages={this.TOAST_QUEUE}
-        closeMessage={this.removeToast}
-      />
-    );
+    this.ROOT_ELEMENT.render(<ToastMessage messages={this.TOAST_QUEUE} closeMessage={this.removeToast} />);
     this.autoRemoveToast(id);
   };
 
   addError = message => {
     const id = v4();
-    if (
-      this.TOAST_QUEUE.filter(toast => toast.message === message).length > 0
-    ) {
+    if (this.TOAST_QUEUE.filter(toast => toast.message === message).length > 0) {
       return;
     }
     this.TOAST_QUEUE.push({
       id,
       message,
-      messageType: 'error'
+      messageType: 'error',
     });
-    this.ROOT_ELEMENT.render(
-      <ToastMessage
-        messages={this.TOAST_QUEUE}
-        closeMessage={this.removeToast}
-      />
-    );
+    this.ROOT_ELEMENT.render(<ToastMessage messages={this.TOAST_QUEUE} closeMessage={this.removeToast} />);
     this.autoRemoveToast(id);
   };
 
   addInfo = message => {
     const id = v4();
-    if (
-      this.TOAST_QUEUE.filter(toast => toast.message === message).length > 0
-    ) {
+    if (this.TOAST_QUEUE.filter(toast => toast.message === message).length > 0) {
       return;
     }
     this.TOAST_QUEUE.push({
       id,
       message,
-      messageType: 'info'
+      messageType: 'info',
     });
-    this.ROOT_ELEMENT.render(
-      <ToastMessage
-        messages={this.TOAST_QUEUE}
-        closeMessage={this.removeToast}
-      />
-    );
+    this.ROOT_ELEMENT.render(<ToastMessage messages={this.TOAST_QUEUE} closeMessage={this.removeToast} />);
     this.autoRemoveToast(id);
   };
 
   removeToast = id => {
     const deleteIdx = this.TOAST_QUEUE.findIndex(toast => toast.id === id);
     this.TOAST_QUEUE.splice(deleteIdx, 1);
-    this.ROOT_ELEMENT.render(
-      <ToastMessage
-        messages={this.TOAST_QUEUE}
-        closeMessage={this.removeToast}
-      />
-    );
+    this.ROOT_ELEMENT.render(<ToastMessage messages={this.TOAST_QUEUE} closeMessage={this.removeToast} />);
   };
 
   autoRemoveToast = id => {
