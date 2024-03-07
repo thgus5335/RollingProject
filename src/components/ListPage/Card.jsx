5315a6a848cf5aadcb9bd5380a55de51f1ce90db
eugene9851 <eugenelee980713@gmail.com>
2024-03-07 16:23:09 +0900

    Fix: Profile common component

--- src/components/ListPage/Card.jsx
+++ src/components/ListPage/Card.jsx
@@ -4,6 +4,7 @@ import bluePattern from '../../assets/images/pattern-blue.png';
 import purplePattern from '../../assets/images/pattern-purple.png';
 import greenPattern from '../../assets/images/pattern-green.png';
 import Emoji from '../common/Emoji';
+import Profile from '../common/Profile';
 
 const backgroundImagePattern = {
   beige: beigePattern,
@@ -25,24 +26,7 @@ const Card = ({ name, messageCount, backgroundImage, backgroundColor, recentMess
     <div style={backgroundImageStyle} className={`${styles[background]} ${styles.cardArea}`}>
       <div className={styles.infoArea}>
         <h3 className={styles.name}>To. {name}</h3>
-
-        {recentMessages && (
-          <div className={styles.imageStyle}>
-            {recentMessages.map((recentMessage, index) => (
-              <img
-                key={recentMessage.id}
-                src={recentMessage.profileImageURL}
-                className={styles.profileImage}
-                style={{ left: `${index * -1}rem` }}
-              />
-            ))}
-            {messageCount > 3 && <div className={styles.plusProfile}>+{messageCount - 3}</div>}
-          </div>
-        )}
-
-        <p className={styles.countDescription}>
-          <span className={styles.strongSpan}>{messageCount}</span>명이 작성했어요!
-        </p>
+        <Profile recentMessages={recentMessages} messageCount={messageCount} />
       </div>
       {emoticon.length !== 0 && (
         <div className={styles.cardBorderLine}>
