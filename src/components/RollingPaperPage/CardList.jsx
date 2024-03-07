d3dc3c6bd1c8bbb9c310a4caca35ee8bb82f9a20
thgus5335 <thgus5335@naver.com>
2024-03-07 13:56:41 +0900

    Fix: delete comment

--- src/components/RollingPaperPage/CardList.jsx
+++ src/components/RollingPaperPage/CardList.jsx
@@ -7,21 +7,10 @@ import { Link } from 'react-router-dom';
 import { useInView } from 'react-intersection-observer';
 
 const RollingPage = ({ id, mode = 'normal' }) => {
-  // const [message, setMessage] = useState([]);
   const editMode = mode === 'edit' ? 'editCard' : '';
-
-  // const fetchData = async (id, limit, offset) => {
-  //   const response = await getMessage(id, limit, offset);
-  //   setMessage(response);
-  // };
-
-  // useEffect(() => {
-  //   fetchData(id);
-  // }, []);
 
   const [offset, setOffset] = useState(6);
   const [messageList, setMessageList] = useState([]);
-  // console.log('MMMMMM', message);
 
   const handleMessageList = async (id, limit, offset) => {
     const response = await getMessage(id, limit, offset);
@@ -29,25 +18,7 @@ const RollingPage = ({ id, mode = 'normal' }) => {
     if (offset === 0) setMessageList(response);
     else setMessageList(prevList => [...prevList, ...response]);
   };
-  // const loadMessages = async (options) => {
-  //   const RESPONSE = await fetchMessagesAsync(options);
 
-  //   if (options.offset === 0) {
-  //     setList(RESPONSE?.results);
-  //   } else {
-  //     setList((prevList) => [...prevList, ...RESPONSE?.results]);
-  //   }
-
-  //   const NEXT = RESPONSE?.next;
-  //   if (NEXT) {
-  //     setOffset(NEXT.split("offset=")[1]);
-  //     setHasNext(true);
-  //   } else {
-  //     setHasNext(false);
-  //   }
-  // };
-  //       setMessage(preData => preData.concat(res.data.results));
-  //       setPage(prePage => prePage + PAGE_SIZE);
   const [ref, inView] = useInView();
   useEffect(() => {
     if (inView) {
@@ -56,27 +27,6 @@ const RollingPage = ({ id, mode = 'normal' }) => {
       handleMessageList(id, 6, offset);
     }
   }, [inView]);
-
-  // useEffect(() => {
-  //   const handleIntersection = entries => {
-  //     entries.forEach(entry => {
-  //       if (entry.isIntersecting && hasNext) {
-  //         loadMessages({ recipientId, offset, limit: LIMIT });
-  //       }
-  //     });
-  //   };
-
-  //   const observer = new IntersectionObserver(handleIntersection, IO_OPTIONS);
-
-  //   observer.observe(SENTINEL.current);
-
-  //   return () => {
-  //     /* 페이지 전환 시 element 사라짐 대응 조건식 */
-  //     if (SENTINEL.current) {
-  //       observer.unobserve(SENTINEL.current);
-  //     }
-  //   };
-  // }, [offset, hasNext]);
 
   return (
     <>
@@ -93,39 +43,3 @@ const RollingPage = ({ id, mode = 'normal' }) => {
 };
 
 export default RollingPage;
-
-// import { useInView } from 'react-intersection-observer';
-// import axios from 'axios';
-// import LIMIT from '../../constants/constants';
-// import useIntersection from '../../hooks/useIntersection';
-
-// //-----------------------------------------------------------------------
-// const [message, setMessage] = useState([]);
-// const [page, setPage] = useState(0);
-// const [loading, setLoading] = useState(false);
-// const [ref, inView] = useInView();
-// const PAGE_SIZE = 6;
-
-// const getMessageList = id => {
-//   setLoading(true);
-
-//   axios
-//     .get(`https://rolling-api.vercel.app/4-13/recipients/${id}/messages/?limit=${PAGE_SIZE}&offset=${page}`)
-//     .then(res => {
-//       setMessage(preData => preData.concat(res.data.results));
-//       setPage(prePage => prePage + PAGE_SIZE);
-//     })
-//     .catch(error => {
-//       alert(error);
-//     })
-//     .finally(() => {
-//       setLoading(false);
-//     });
-// };
-
-// useEffect(() => {
-//   if (inView && page > 0) {
-//     console.log(inView, '무한 스크롤');
-//     getMessageList(id);
-//   }
-// }, [inView]);
