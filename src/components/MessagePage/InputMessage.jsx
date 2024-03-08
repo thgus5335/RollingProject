80c84c5ab77b699a891dc86c7186f82d53d55353
hyunjun9788 <namhyunjun9788@gmail.com>
2024-03-08 15:32:47 +0900

    Feat: Apply React Quill Toolbar Functionality

--- src/components/MessagePage/InputMessage.jsx
+++ src/components/MessagePage/InputMessage.jsx
@@ -1,13 +1,11 @@
 import styles from './InputMessage.module.css';
-import ReactQuill from 'react-quill';
+import QuillEditor from './QuillEditor';
+
 const InputMessage = ({ onMessageChange }) => {
-  const handleChangeContent = text => {
-    onMessageChange(text);
-  };
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>내용을 입력해 주세요.</h1>
-      <ReactQuill onChange={handleChangeContent} style={{ height: '19.25rem' }} />
+      <QuillEditor onMessageChange={onMessageChange} />
     </div>
   );
 };
