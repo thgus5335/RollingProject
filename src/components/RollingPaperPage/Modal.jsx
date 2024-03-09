e32274bc49eb15dfe80ee28158a509b5f7addaa9
thgus5335 <thgus5335@naver.com>
2024-03-09 11:26:50 +0900

    Fix: separate card modal

--- src/components/RollingPaperPage/Modal.jsx
+++ src/components/RollingPaperPage/Modal.jsx
@@ -1,31 +1,12 @@
-import Button from '../common/Button';
 import styles from './Modal.module.css';
-import ManageMessage from '../../utils/ManageMessage';
+import ModalCard from './ModalCard';
 
 const Modal = ({ onClose, messageInfo }) => {
-  const { profileImageURL, sender, relationship, content, fontStyle, badge, date } = ManageMessage(messageInfo);
   return (
     <>
       <div className={styles.modalMask} onClick={onClose}></div>
       <div className={styles.modalContainer} onClick={event => event.stopPropagation()}>
-        <div className={styles.profile}>
-          <div className={styles.profileInfo}>
-            <div className={styles.profileLeft}>
-              <img className={styles.profileImage} src={profileImageURL} alt="보낸 사람의 프로필." />
-              <div>
-                <p className={styles.profileTitle}>From. {sender}</p>
-                <p className={`${styles.profileBadge} ${styles[badge[relationship]]}`}>{relationship}</p>
-              </div>
-            </div>
-            <p className={styles.date}>{date}</p>
-          </div>
-        </div>
-        <div className={styles.content}>
-          <p className={`${styles.message} ${styles[fontStyle]}`} dangerouslySetInnerHTML={{ __html: content }} />
-          <div className={styles.buttonOk} onClick={onClose}>
-            <Button size="medium">확인</Button>
-          </div>
-        </div>
+        <ModalCard onClose={onClose} messageInfo={messageInfo} />
       </div>
     </>
   );
---
+++ src/components/RollingPaperPage/ModalCard.jsx
@@ -0,0 +1,31 @@
+import Button from '../common/Button';
+import styles from './ModalCard.module.css';
+import ManageMessage from '../../utils/ManageMessage';
+
+const ModalCard = ({ onClose, messageInfo }) => {
+  const { profileImageURL, sender, relationship, content, fontStyle, badge, date } = ManageMessage(messageInfo);
+  return (
+    <>
+      <div className={styles.profile}>
+        <div className={styles.profileInfo}>
+          <div className={styles.profileLeft}>
+            <img className={styles.profileImage} src={profileImageURL} alt="보낸 사람의 프로필." />
+            <div>
+              <p className={styles.profileTitle}>From. {sender}</p>
+              <p className={`${styles.profileBadge} ${styles[badge[relationship]]}`}>{relationship}</p>
+            </div>
+          </div>
+          <p className={styles.date}>{date}</p>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={`${styles.message} ${styles[fontStyle]}`} dangerouslySetInnerHTML={{ __html: content }} />
+        <div className={styles.buttonOk} onClick={onClose}>
+          <Button size="medium">확인</Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ModalCard;
