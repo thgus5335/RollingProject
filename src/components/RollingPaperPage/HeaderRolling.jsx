80be3526bef6f5ff0f8f3ac38e4e2ec76649f72a
정기환 <joelkh784@gmail.com>
2024-03-10 13:22:20 +0900

    Feat: click outside to collapse emoji drop down and emojiPicker

--- src/components/RollingPaperPage/HeaderRolling.jsx
+++ src/components/RollingPaperPage/HeaderRolling.jsx
@@ -16,14 +16,14 @@ import { ToastContainer } from 'react-toastify';
 const HeaderRolling = ({ rollingInfo }) => {
   const [isEmojiClicked, setIsEmojiClicked] = useState(false);
   const [emojiDropDown, setEmojiDropDown] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
+  const [topEmojis, setTopEmojis] = useState([]);
+  const [emojiList, setEmojiList] = useState([]);
 
   const recipient = rollingInfo.name;
   const writer = rollingInfo.messageCount;
   const recentMessages = rollingInfo.recentMessages;
   const id = rollingInfo.id;
-
-  const [topEmojis, setTopEmojis] = useState([]);
-  const [emojiList, setEmojiList] = useState([]);
 
   const fetchEmoji = async id => {
     const response = await getReaction(id);
@@ -35,17 +35,14 @@ const HeaderRolling = ({ rollingInfo }) => {
     setTopEmojis(response);
   };
 
-  const [dropdown, setDropdown] = useState(false);
-  const handleDropdown = () => {
-    setDropdown(true);
-  };
-
-  const handleClickShareURL = async () => {
-    const url = window.location.href;
-    await navigator.clipboard.writeText(url);
-  };
   const shareRef = useRef(null);
   useClickOutside(shareRef, setDropdown);
+
+  const emojiDropDownRef = useRef(null);
+  useClickOutside(emojiDropDownRef, setEmojiDropDown);
+
+  const emojiPickerRef = useRef(null);
+  useClickOutside(emojiPickerRef, setIsEmojiClicked);
 
   const handleButtonClick = () => {
     setIsEmojiClicked(prev => !prev);
@@ -54,6 +51,15 @@ const HeaderRolling = ({ rollingInfo }) => {
   const handleEmojiDropDownClick = () => {
     setEmojiDropDown(prev => !prev);
     fetchEmoji(id);
+  };
+
+  const handleDropdown = () => {
+    setDropdown(prev => !prev);
+  };
+
+  const handleClickShareURL = async () => {
+    const url = window.location.href;
+    await navigator.clipboard.writeText(url);
   };
 
   const onEmojiClick = emoji => {
@@ -71,6 +77,12 @@ const HeaderRolling = ({ rollingInfo }) => {
 
   return (
     <>
+      <ToastContainer
+        toastStyle={{
+          backgroundColor: 'black',
+          color: 'white',
+        }}
+      />
       <div className={styles.rollingMobileHeader}>
         <div className={styles.recipientMobile}>To. {recipient}</div>
       </div>
@@ -81,7 +93,7 @@ const HeaderRolling = ({ rollingInfo }) => {
             <div className={styles.headerProfile}>
               <Profile recentMessages={recentMessages} messageCount={writer} />
             </div>
-            <div className={styles.emojiDropDownContainer}>
+            <div className={styles.emojiDropDownContainer} ref={emojiDropDownRef}>
               <div className={styles.emojiArea}>{topEmojis && <Emoji emoticon={topEmojis} />}</div>
               {topEmojis.length > 0 && (
                 <img
@@ -94,23 +106,17 @@ const HeaderRolling = ({ rollingInfo }) => {
             </div>
             {emojiDropDown && <div className={styles.emojiDropDown}>{emojiList && <Emoji emoticon={emojiList} />}</div>}
             <div className={styles.buttonContainer}>
-              <div>
+              <div ref={emojiPickerRef}>
                 <Button className={styles.emojiButton} onClick={handleButtonClick} size="small" type="outline">
                   <img src={emojiIcon} alt="emoji icon" />
                   <span className={styles.addEmojiButton}>추가</span>
-                  {isEmojiClicked && (
-                    <div className={styles.emojiContainer}>
-                      <EmojiPicker onEmojiClick={onEmojiClick} />
-                    </div>
-                  )}
                 </Button>
+                {isEmojiClicked && (
+                  <div className={styles.emojiContainer}>
+                    <EmojiPicker onEmojiClick={onEmojiClick} />
+                  </div>
+                )}
               </div>
-              <ToastContainer
-                toastStyle={{
-                  backgroundColor: 'black',
-                  color: 'white',
-                }}
-              />
               <div className={styles.line}></div>
               <div className={styles.dropDownWrapper} ref={shareRef}>
                 <Button size="small" type="outline" onClick={handleDropdown}>
